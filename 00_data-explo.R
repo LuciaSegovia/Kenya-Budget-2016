20efdf1fedf13ae2bf1d4f6a9594e06329131704
Lucia Segovia De La Revilla <lshls9@lshtm.ac.uk>
2025-04-15 15:01:06 +0100

    starting exploring variables

---
+++ 00_data-explo.R
@@ -0,0 +1,62 @@
+
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+# Data
+# Food consumption at HHs (7days)
+df <- haven::read_dta(here::here("data", "food.dta")) %>% 
+  rename(
+         purchased = "t04_qy", 
+         purchased_unit = "t04_su", 
+         pop = "t05",  # point of purchase
+         cons_purch = "t06_qy",
+         cons_purch_unit = "t06_su",
+         cons_stock = "t07_qy",
+         cons_stock_unit = "t07_su",
+         cons_own = "t08_qy",
+         cons_own_unit = "t08_su",
+         cons_gift = "t09_qy",
+         cons_gidt_unit = "t09_su",
+         cons_total = "t10_qy",
+         cons_unit = "t10_su"
+         )
+# Best to explore the meaning of variables in stata dataset.
+str(df) 
+# Roster info
+hh <- haven::read_dta(here::here("data", "HH_Members_Information.dta")) %>% 
+  rename(sex = "", 
+         age_y = "", 
+         school = "c03", 
+         school_km = "c04", 
+         school_feed = "c05",
+         school_level = "c06_l", 
+         school_grade = "c06_g", 
+         drop_reason1 = "c09_r1",
+         drop_reason2 = "c09_R2",
+         sickness1 = "hh$e03_1", # 26 pregnancy related
+         sickness2 = "hh$e03_2", # 26 pregnancy related
+         weight_kg = "f21", 
+         height_cm = "f22", 
+         )
+# Expenditure aggregate HHs
+haven::read_dta(here::here("data", "Consumption_aggregate.dta"))
+names(haven::read_dta(here::here("data", "food.dta")))
+# Agriculture outputs (12 months) (Section L of Q1B)
+names(haven::read_dta(here::here("data", "Agriculture_output_L1_L20.dta")))
+df3 <- haven::read_dta(here::here("data", "Agriculture_output_L1_L20.dta")) %>% 
+  filter(k01 == 1)
+
+# Checkiing food items and NSU
+sort(unique(haven::as_factor(df$item_code)))
+unique(haven::as_factor(df$t04_sq))
+unique(haven::as_factor(df3$l02_cr))
+
+
+
+haven::read_dta(here::here("data", "Consumption_aggregate.dta")) %>% 
+  mutate(hhsize_cat = as.factor(ifelse(hhsize>=10, "10+", hhsize)), 
+         edu = padqeduc/hhsize) %>% 
+  ggplot(aes(reorder(edu, hhsize_cat), edu)) + geom_violin() +
+  coord_flip()
